f4ca4c21c62c2b1bc329f49196a9a8fc3bf8baf7
AEscobar516 <55443534+AEscobar516@users.noreply.github.com>
2020-03-13 08:05:00 -0400

    I think we created more Text mesh pro's... that's it

---
+++ Stackeys/Assets/Scoretracker.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreTracker : MonoBehaviour
+{
+public class ScoreTracker INSTANCE;
+public int bestscore = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
